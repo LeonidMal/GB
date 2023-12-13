35e1a47a0461f7f9f408ee8c84ea8ad1a4699463
Leonid <rockwil91@gmail.com>
2023-12-13 04:30:28 +0200

    JAVA закончил лекцию 2

--- IT_COURSE/JAVA_COURSE/Seminar/one/Six.java
+++ IT_COURSE/JAVA_COURSE/Seminar/one/Six.java
@@ -21,7 +21,7 @@ public class Six {
     }
     static double pow_num(double num1, int num2) {
         
-        if (num1 == 1 || num2 ==0) {
+        if (num1 == 1 || num2 == 0) {
             return 1;
         }
 
